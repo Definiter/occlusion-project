b722a32fd1f4103f536c754e9fababe6a7f8a0a5
Hao Wang <definiter@gmail.com>
2016-09-10 15:43:17 -0400

    Adjust for paper writing.

--- script/img2vec_all.sh
+++ script/img2vec_all.sh
@@ -1,6 +1,6 @@
-model_type_strs=(crop_obj)
+model_type_strs=(every)
 model_names=(all)
-test_type_strs=(crop aperture)
+test_type_strs=(aperture crop_obj)
 test_names=(0 10 20 30 40 50 60 70 80 90 100)
 
 for model_type_str in "${model_type_strs[@]}"
@@ -20,19 +20,19 @@ do
             sleep 10
             qsub -q taising -v gpu=3,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=30 -l nodes=compute-1-1  -N ${model_type_str}_${model_name}_${test_type_str}_30_img2vec pbs_img2vec.script
             sleep 10
-            qsub -q taising -v gpu=0,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=40 -l nodes=compute-1-3  -N ${model_type_str}_${model_name}_${test_type_str}_40_img2vec pbs_img2vec.script
+            qsub -q taising -v gpu=0,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=40 -l nodes=compute-1-5  -N ${model_type_str}_${model_name}_${test_type_str}_40_img2vec pbs_img2vec.script
             sleep 10
-            qsub -q taising -v gpu=1,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=50 -l nodes=compute-1-3  -N ${model_type_str}_${model_name}_${test_type_str}_50_img2vec pbs_img2vec.script
+            qsub -q taising -v gpu=1,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=50 -l nodes=compute-1-5  -N ${model_type_str}_${model_name}_${test_type_str}_50_img2vec pbs_img2vec.script
             sleep 10
-            qsub -q taising -v gpu=2,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=60 -l nodes=compute-1-3  -N ${model_type_str}_${model_name}_${test_type_str}_60_img2vec pbs_img2vec.script
+            qsub -q taising -v gpu=2,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=60 -l nodes=compute-1-5  -N ${model_type_str}_${model_name}_${test_type_str}_60_img2vec pbs_img2vec.script
             sleep 10
-            qsub -q taising -v gpu=3,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=70 -l nodes=compute-1-3  -N ${model_type_str}_${model_name}_${test_type_str}_70_img2vec pbs_img2vec.script
+            qsub -q taising -v gpu=3,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=70 -l nodes=compute-1-5  -N ${model_type_str}_${model_name}_${test_type_str}_70_img2vec pbs_img2vec.script
             sleep 10
             qsub -q taising -v gpu=0,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=80 -l nodes=compute-1-1  -N ${model_type_str}_${model_name}_${test_type_str}_80_img2vec pbs_img2vec.script
             sleep 10
-            qsub -q taising -v gpu=1,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=90 -l nodes=compute-1-5  -N ${model_type_str}_${model_name}_${test_type_str}_90_img2vec pbs_img2vec.script
+            qsub -q taising -v gpu=1,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=90 -l nodes=compute-1-1  -N ${model_type_str}_${model_name}_${test_type_str}_90_img2vec pbs_img2vec.script
             sleep 10
-            qsub -q taising -v gpu=2,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=100 -l nodes=compute-1-5  -N ${model_type_str}_${model_name}_${test_type_str}_100_img2vec pbs_img2vec.script
+            qsub -q taising -v gpu=2,model_type_str=${model_type_str},model_name=${model_name},test_type_str=${test_type_str},test_name=100 -l nodes=compute-1-1  -N ${model_type_str}_${model_name}_${test_type_str}_100_img2vec pbs_img2vec.script
             sleep 10
             echo ---
         done
--- script/lmdb_all.sh
+++ script/lmdb_all.sh
@@ -1,5 +1,5 @@
-func_strs=(test)
-type_strs=(crop nocrop)
+func_strs=(train val test)
+type_strs=(every)
 names=(0 10 20 30 40 50 60 70 80 90 100)
 
 for func_str in "${func_strs[@]}"
@@ -12,17 +12,11 @@ do
             echo $func_str
             echo $type_str
             #echo $name
-            qsub -q default -v func_str=${func_str},type_str=${type_str},name=10 -l nodes=compute-0-10 -N ${func_str}_${type_str}_10_lmdb pbs_lmdb.script
-            sleep 30
-            qsub -q default -v func_str=${func_str},type_str=${type_str},name=30 -l nodes=compute-0-11 -N ${func_str}_${type_str}_30_lmdb pbs_lmdb.script
-            sleep 30
-            qsub -q default -v func_str=${func_str},type_str=${type_str},name=50 -l nodes=compute-0-12 -N ${func_str}_${type_str}_50_lmdb pbs_lmdb.script
-            sleep 30
-            qsub -q default -v func_str=${func_str},type_str=${type_str},name=70 -l nodes=compute-0-13 -N ${func_str}_${type_str}_70_lmdb pbs_lmdb.script
-            sleep 30
-            qsub -q default -v func_str=${func_str},type_str=${type_str},name=90 -l nodes=compute-0-14 -N ${func_str}_${type_str}_90_lmdb pbs_lmdb.script
-            sleep 30
+            #qsub -q default -v func_str=${func_str},type_str=${type_str},name=all -l nodes=compute-0-10 -N ${func_str}_${type_str}_all_lmdb pbs_lmdb.script
             echo ---------
         #done
     done
 done
+qsub -q default -v func_str=train,type_str=every,name=all -l nodes=compute-0-10 -N train_every_all_lmdb pbs_lmdb.script
+qsub -q default -v func_str=val,type_str=every,name=all -l nodes=compute-0-11 -N val_every_all_lmdb pbs_lmdb.script
+qsub -q default -v func_str=test,type_str=every,name=all -l nodes=compute-0-12 -N test_every_all_lmdb pbs_lmdb.script
