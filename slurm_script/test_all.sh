5c8a861ac13bacd73dd26103c263bdb514aed241
Hao Wang <definiter@gmail.com>
2017-05-24 17:30:23 -0400

    CRV.

--- slurm_script/test_all.sh
+++ slurm_script/test_all.sh
@@ -1,7 +1,7 @@
 model_type_strs=(crop_image)
 model_names=(all)
 #test_type_strs=(crop crop_img crop_obj crop_image aperture)
-test_type_strs=(crop)
+test_type_strs=(crop_img)
 
 for model_type_str in "${model_type_strs[@]}"
 do
@@ -12,17 +12,17 @@ do
             echo $model_type_str
             echo $model_name
             echo $test_type_str
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=0 --nodelist=compute-1-5 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=1 --nodelist=compute-1-5 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=2 --nodelist=compute-1-5 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=3 --nodelist=compute-1-5 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=4 --nodelist=compute-1-7 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=5 --nodelist=compute-1-7 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=6 --nodelist=compute-1-7 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=7 --nodelist=compute-1-7 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=8 --nodelist=compute-1-9 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=9 --nodelist=compute-1-9 slurm_test.script
-            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=10 --nodelist=compute-1-9 slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=0 --nodelist=compute-1-5  --output=${model_type_str}_${model_name}_${test_type_str}_0_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=10 --nodelist=compute-1-7 --output=${model_type_str}_${model_name}_${test_type_str}_10_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=20 --nodelist=compute-1-7 --output=${model_type_str}_${model_name}_${test_type_str}_20_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=30 --nodelist=compute-1-5 --output=${model_type_str}_${model_name}_${test_type_str}_30_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=40 --nodelist=compute-1-7 --output=${model_type_str}_${model_name}_${test_type_str}_40_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=50 --nodelist=compute-1-7 --output=${model_type_str}_${model_name}_${test_type_str}_50_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=60 --nodelist=compute-1-7 --output=${model_type_str}_${model_name}_${test_type_str}_60_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=70 --nodelist=compute-1-7 --output=${model_type_str}_${model_name}_${test_type_str}_70_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=80 --nodelist=compute-1-9 --output=${model_type_str}_${model_name}_${test_type_str}_80_test.out slurm_test.script
+            #sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=90 --nodelist=compute-1-7 --output=${model_type_str}_${model_name}_${test_type_str}_90_test.out slurm_test.script
+            sbatch --export=gpu=0,model_type_str=$model_type_str,model_name=$model_name,test_type_str=$test_type_str,test_name=100 --nodelist=compute-1-9 --output=${model_type_str}_${model_name}_${test_type_str}_100_test.out  slurm_test.script
             echo ---
         done
     done
